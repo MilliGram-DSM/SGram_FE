ba3c7d4d302f039a7d00d562f463933526703e33
정승우 <ssounsowns@gmail.com>
2024-08-04 14:17:52 +0900

    기본 컴포넌트들 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
+import { createBrowserRouter } from 'react-router-dom'
+import Login from './routes/Login'
+
+const router = createBrowserRouter([{}])
 
 
 function App() {
 
   return (
-  <></>
+  <><Login/></>
   )
 }
 
